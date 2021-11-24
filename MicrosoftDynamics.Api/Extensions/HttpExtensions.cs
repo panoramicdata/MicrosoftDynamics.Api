560a891b181e5ec70f767420bf29032fbdeba41d
David Bond <david.bond@panoramicdata.com>
2021-11-24 01:36:56 +0000

    Fixed broken XML output

--- MicrosoftDynamics.Api/Extensions/HttpExtensions.cs
+++ MicrosoftDynamics.Api/Extensions/HttpExtensions.cs
@@ -6,7 +6,13 @@ internal static class HttpExtensions
 		=> string.Join("\n", headers.Select(h => $"{h.Key}={string.Join(", ", h.Value)}"));
 
 	internal static async Task<string> ToDebugStringAsync(this HttpContent content)
-		=> content is null ? "No content" : FormatJson(await content.ReadAsStringAsync());
+	{
+		string contentString;
+		return
+			content is null ? "No content" :
+			(contentString = await content.ReadAsStringAsync()).StartsWith("{") ? FormatJson(contentString)
+			: contentString;
+	}
 
 	private static string FormatJson(string json)
 		=> JsonConvert.SerializeObject(JsonConvert.DeserializeObject(json), Formatting.Indented);
