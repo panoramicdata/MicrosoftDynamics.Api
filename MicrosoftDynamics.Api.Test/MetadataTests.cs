c9be2196ecd756840750fed11c87c2864940e776
David Bond <david.bond@panoramicdata.com>
2024-06-06 17:49:28 +0100

    Added metadata unit test

---
+++ MicrosoftDynamics.Api.Test/MetadataTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.OData.Edm;
+
+namespace MicrosoftDynamics.Api.Test;
+
+public class MetadataTests(ITestOutputHelper testOutputHelper) : TestBase(testOutputHelper)
+{
+	[Fact]
+	public async Task GetMetadata_Succeeds()
+	{
+		var result = await Client
+			.GetMetadataAsync(default)
+			.ConfigureAwait(true);
+
+		var model = result as IEdmModel;
+
+		model.Should().NotBeNull();
+	}
+}
